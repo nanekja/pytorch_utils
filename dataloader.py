81bbc5db4a1216d238c84ebc0eaf22f8fb96fc91
“nanekja” <raja.nannapaneni@gmail.com>
2023-07-03 22:31:33 +0530

    utils1

---
+++ dataloader.py
@@ -0,0 +1,15 @@
+import torchvision
+
+class Cifar10Dataset(torchvision.datasets.CIFAR10):
+    def __init__(self, root="~/data/cifar10", train=True, download=True, transform=None):
+        super().__init__(root=root, train=train, download=download, transform=transform)
+
+    def __getitem__(self, index):
+        image, label = self.data[index], self.targets[index]
+
+        if self.transform is not None:
+            transformed = self.transform(image=image)
+            image = transformed["image"]
+
+        image = np.transpose(image, (2,0,1)).astype(np.float32)
+        return torch.tensor(image, dtype=torch.float), label
---
+++ train.py
@@ -0,0 +1,57 @@
+import numpy as np
+import torch
+from tqdm import tqdm
+import torch.nn.functional as F
+
+def train(model, device, train_loader, optimizer,scheduler):
+    """Model Training Loop
+    Args:
+        model : torch model 
+        device : "cpu" or "cuda" gpu 
+        train_loader : Torch Dataloader for trainingset
+        optimizer : optimizer to be used
+    Returns:
+        float: accuracy and loss values
+    """
+    model.train()
+    pbar = tqdm(train_loader)
+    lr_trend = []
+    correct = 0
+    processed = 0
+    num_loops = 0
+    train_loss = 0
+    for batch_idx, (data, target) in enumerate(pbar):
+            # get samples
+        data, target = data.to(device), target.to(device)
+
+        # Init
+        optimizer.zero_grad()
+        # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch 
+        # accumulates the gradients on subsequent backward passes. Because of this, when you start your training loop, 
+        # ideally you should zero out the gradients so that you do the parameter update correctly.
+
+        # Predict
+        y_pred = model(data)
+        # Calculate loss
+        loss = F.nll_loss(y_pred, target)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+
+        scheduler.step()
+        lr_trend.append(scheduler.get_last_lr()[0])
+
+        train_loss += loss.item()
+        
+        # Update pbar-tqdm
+        
+        pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        processed += len(data)
+
+        num_loops += 1
+        pbar.set_description(desc= f'Batch_id={batch_idx} Loss={train_loss/num_loops:.5f} Accuracy={100*correct/processed:0.2f}')
+
+    return 100*correct/processed, train_loss/num_loops, lr_trend
+
