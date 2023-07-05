4027c59b6fed43b98bd8604d13b6ba389f3465f5
“nanekja” <raja.nannapaneni@gmail.com>
2023-07-05 10:22:46 +0530

    modified:   utils.py

--- utils.py
+++ utils.py
@@ -16,6 +16,30 @@ def get_mean_and_std(exp_data):
     print(' - Var:', np.var(exp_data, axis=(0,1,2)) / 255.)
     return np.mean(exp_data, axis=(0,1,2)) / 255., np.std(exp_data, axis=(0,1,2)) / 255.
 
+
+def plot_data(data, rows, cols):
+    """Randomly plot the images from the dataset for vizualization
+
+    Args:
+        data (instance): torch instance for data loader
+        rows (int): number of rows in the plot
+        cols (int): number of cols in the plot
+    """
+    figure = plt.figure(figsize=(cols*2,rows*3))
+    for i in range(1, cols*rows + 1):
+        k = np.random.randint(0,50000)
+        figure.add_subplot(rows, cols, i) # adding sub plot
+
+        img, label = data[k]
+        
+        plt.imshow(img)
+        plt.axis('off')
+        plt.title(f'Class: {label} '+data.classes[label])
+
+    plt.tight_layout()
+    plt.show()
+
+
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
