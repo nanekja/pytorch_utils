8699ed2b14dd0e4bca37593f0e33c64d52d19f05
“nanekja” <raja.nannapaneni@gmail.com>
2023-07-07 20:58:06 +0530

    c5

--- test1.py
+++ test1.py
@@ -23,7 +23,7 @@ def test(model, device, test_loader, criterion):
             data, target = data.to(device), target.to(device)
             output = model(data)
             #test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            test_loss += criterion(output, target, reduction='sum').item()  # sum up batch loss
+            test_loss += criterion(output, target).item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
