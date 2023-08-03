7bdeae8a8e2c4e01e9874ffff181b07a9b92db64
nanekja <raja.nannapaneni@gmail.com>
2023-08-03 12:32:48 +0530

    c2

--- models/c_resnet.py
+++ models/c_resnet.py
@@ -12,13 +12,13 @@ class Net(nn.Module):
         super(Net, self).__init__()
         
         ## Prep Layer
-        self.prep_layer = nn.Sequential(
+        self.layer1 = nn.Sequential(
             nn.Conv2d(3, 64, 3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(),
         )
 
-        self.layer1 = nn.Sequential(
+        self.layer2 = nn.Sequential(
             nn.Conv2d(64, 128, 3, stride=1, padding=1, bias=False),
             nn.MaxPool2d(2, 2),
             nn.BatchNorm2d(128),
@@ -27,14 +27,14 @@ class Net(nn.Module):
 
         self.resblock1 = self.resblock(128, 128, 3)
 
-        self.layer2 = nn.Sequential(
+        self.layer3 = nn.Sequential(
             nn.Conv2d(128, 256, 3, stride=1, padding=1, bias=False),
             nn.MaxPool2d(2, 2),
             nn.BatchNorm2d(256),
             nn.ReLU()
         )
 
-        self.layer3 = nn.Sequential(
+        self.layer4 = nn.Sequential(
             nn.Conv2d(256, 512, 3, stride=1, padding=1, bias=False),
             nn.MaxPool2d(2, 2),
             nn.BatchNorm2d(512),
@@ -61,15 +61,15 @@ class Net(nn.Module):
 
         
     def forward(self, x):
-        x = self.prep_layer(x) ## Input size = 32x32, output size = 32x32
+        x = self.layer1(x) ## Input size = 32x32, output size = 32x32
         
-        x = self.layer1(x) ## Input size = 32x32, output size = 16x16
+        x = self.layer2(x) ## Input size = 32x32, output size = 16x16
         res_1 = self.resblock1(x) ## Input size = 16x16, output size = 16x16
         x = x + res_1
         
-        x = self.layer2(x) ## Input size = 16x16, output size = 8x8
+        x = self.layer3(x) ## Input size = 16x16, output size = 8x8
         
-        x = self.layer3(x) ## Input size = 8x8, output size = 4x4
+        x = self.layer4(x) ## Input size = 8x8, output size = 4x4
         res_2 = self.resblock2(x) ## Input size = 4x4, output size = 4x4
         x = x + res_2 
         
