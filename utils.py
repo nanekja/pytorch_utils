fd8222dc8f0936dfaa2e9ba8c79ad08c95128c4d
nanekja <raja.nannapaneni@gmail.com>
2023-08-03 19:08:12 +0530

    c3

--- utils.py
+++ utils.py
@@ -13,86 +13,6 @@ import math
 from pytorch_grad_cam.utils.image import show_cam_on_image
 from pytorch_grad_cam import GradCAM
 
-class GradCAM:
-    """Calculate GradCAM salinecy map.
-    Args:
-        input: input image with shape of (1, 3, H, W)
-        class_idx (int): class index for calculating GradCAM.
-                If not specified, the class index that makes the highest model prediction score will be used.
-    Return:
-        mask: saliency map of the same spatial dimension with input
-        logit: model output
-    A simple example:
-        # initialize a model, model_dict and gradcam
-        resnet = torchvision.models.resnet101(pretrained=True)
-        resnet.eval()
-        gradcam = GradCAM.from_config(model_type='resnet', arch=resnet, layer_name='layer4')
-        # get an image and normalize with mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
-        img = load_img()
-        normed_img = normalizer(img)
-        # get a GradCAM saliency map on the class index 10.
-        mask, logit = gradcam(normed_img, class_idx=10)
-        # make heatmap from mask and synthesize saliency map using heatmap and img
-        heatmap, cam_result = visualize_cam(mask, img)
-    """
-
-    def __init__(self, model, layer_name):
-        self.model = model
-        # self.layer_name = layer_name
-        self.target_layer = layer_name
-
-        self.gradients = dict()
-        self.activations = dict()
-
-        def backward_hook(module, grad_input, grad_output):
-            self.gradients['value'] = grad_output[0]
-
-        def forward_hook(module, input, output):
-            self.activations['value'] = output
-
-        self.target_layer.register_forward_hook(forward_hook)
-        self.target_layer.register_backward_hook(backward_hook)
-
-    @classmethod
-    def from_config(cls, arch: torch.nn.Module, model_type: str, layer_name: str):
-        target_layer = layer_finders[model_type](arch, layer_name)
-        return cls(arch, target_layer)
-
-    def saliency_map_size(self, *input_size):
-        device = next(self.model_arch.parameters()).device
-        self.model(torch.zeros(1, 3, *input_size, device=device))
-        return self.activations['value'].shape[2:]
-
-    def forward(self, input, class_idx=None, retain_graph=False):
-        b, c, h, w = input.size()
-
-        logit = self.model(input)
-        if class_idx is None:
-            score = logit[:, logit.max(1)[-1]].squeeze()
-        else:
-            score = logit[:, class_idx].squeeze()
-
-        self.model.zero_grad()
-        score.backward(retain_graph=retain_graph)
-        gradients = self.gradients['value']
-        activations = self.activations['value']
-        b, k, u, v = gradients.size()
-
-        alpha = gradients.view(b, k, -1).mean(2)
-        weights = alpha.view(b, k, 1, 1)
-
-        saliency_map = (weights*activations).sum(1, keepdim=True)
-        saliency_map = F.relu(saliency_map)
-        saliency_map = F.upsample(saliency_map, size=(h, w), mode='bilinear', align_corners=False)
-        saliency_map_min, saliency_map_max = saliency_map.min(), saliency_map.max()
-        saliency_map = (saliency_map - saliency_map_min).div(saliency_map_max - saliency_map_min).data
-        
-        self.gradients.clear()
-        self.activations.clear()
-        return saliency_map, logit
-    def __call__(self, input, class_idx=None, retain_graph=False):
-        return self.forward(input, class_idx, retain_graph)
-
 
 """VISUALIZE_GRADCAM"""
 
@@ -261,6 +181,62 @@ def plot_misclassified(model, test_loader, classes, device, dataset_mean, datase
     if return_misclf:
         return misclf
 
+def plot_misclassified2(model, test_loader, classes, device, dataset_mean, dataset_std, no_misclf, plot_size, return_misclf=False):
+    """Plot the images are wrongly clossified by model
+
+    Args:
+        model (instance): torch instance of defined model (pre trained)
+        test_loader (instace): torch data loader of testing set
+        classes (dict or list): classes in the dataset
+                if dict:
+                    key - class id
+                    value - as class name
+                elif list:
+                    index of list correspond to class id and name
+        device (str): 'cpu' or 'cuda' device to be used
+        dataset_mean (tensor or np array): mean of dataset
+        dataset_std (tensor or np array): std of dataset
+        no_misclf (int, optional): number of misclassified images to plot. Defaults to 20.
+        plot_size (tuple): tuple containing size of plot as rows, columns. Defaults to (4,5)
+        return_misclf (bool, optional): True to return the misclassified images. Defaults to False.
+
+    Returns:
+        list: list containing misclassified images as np array if return_misclf True
+    """
+    count = 0
+    k = 0
+    misclf = list()
+  
+    while count<no_misclf:
+        img_model, label = test_loader.dataset[k]
+        pred = model(img_model.unsqueeze(0).to(device)) # Prediction
+        # pred = model(img.unsqueeze(0).to(device)) # Prediction
+        pred = pred.argmax().item()
+
+        k += 1
+        if pred!=label:
+            img = convert_image_np(
+                img_model, dataset_mean, dataset_std)
+            misclf.append((img_model, img, label, pred))
+            count += 1
+    
+    rows, cols = plot_size
+    figure = plt.figure(figsize=(cols*3,rows*3))
+
+    for i in range(1, cols * rows + 1):
+        _, img, label, pred = misclf[i-1]
+
+        figure.add_subplot(rows, cols, i) # adding sub plot
+        plt.title(f"Pred label: {classes[pred]}\n True label: {classes[label]}") # title of plot
+        plt.axis("off") # hiding the axis
+        plt.imshow(img, cmap="gray") # showing the plot
+
+    plt.tight_layout()
+    plt.show()
+    
+    if return_misclf:
+        return misclf
+
 def convert_image_np(inp, mean, std):
     """Convert normalized tensor to numpy image for display.
 
@@ -303,119 +279,8 @@ def ler_rate(net, optimizer, criterion, train_loader):
     return lr1
 
 
-def get_misclassified_data(model, device, test_loader):
-    """
-    Function to run the model on test set and return misclassified images
-    :param model: Network Architecture
-    :param device: CPU/GPU
-    :param test_loader: DataLoader for test set
-    """
-    # Prepare the model for evaluation i.e. drop the dropout layer
-    model.eval()
-
-    # List to store misclassified Images
-    misclassified_data = []
-
-    # Reset the gradients
-    with torch.no_grad():
-        # Extract images, labels in a batch
-        for data, target in test_loader:
-
-            # Migrate the data to the device
-            data, target = data.to(device), target.to(device)
-
-            # Extract single image, label from the batch
-            for image, label in zip(data, target):
-
-                # Add batch dimension to the image
-                image = image.unsqueeze(0)
-
-                # Get the model prediction on the image
-                output = model(image)
-
-                # Convert the output from one-hot encoding to a value
-                pred = output.argmax(dim=1, keepdim=True)
-
-                # If prediction is incorrect, append the data
-                if pred != label:
-                    misclassified_data.append((image, label, pred))
-    return misclassified_data
-
-# -------------------- GradCam --------------------
-def display_gradcam_output(data: list,
-                           classes: list[str],
-                           inv_normalize: transforms.Normalize,
-                           model,
-                           #model: 'DL Model',
-                           target_layers: list['model_layer'],
-                           targets=None,
-                           number_of_samples: int = 10,
-                           transparency: float = 0.60):
-    """
-    Function to visualize GradCam output on the data
-    :param data: List[Tuple(image, label)]
-    :param classes: Name of classes in the dataset
-    :param inv_normalize: Mean and Standard deviation values of the dataset
-    :param model: Model architecture
-    :param target_layers: Layers on which GradCam should be executed
-    :param targets: Classes to be focused on for GradCam
-    :param number_of_samples: Number of images to print
-    :param transparency: Weight of Normal image when mixed with activations
-    """
-    # Plot configuration
-    fig = plt.figure(figsize=(10, 10))
-    x_count = 5
-    y_count = 1 if number_of_samples <= 5 else math.floor(number_of_samples / x_count)
-
-    # Create an object for GradCam
-    cam = GradCAM(model=model, target_layers=target_layers, use_cuda=True)
-
-    # Iterate over number of specified images
-    for i in range(number_of_samples):
-        plt.subplot(y_count, x_count, i + 1)
-        input_tensor = data[i][0]
-
-        # Get the activations of the layer for the images
-        grayscale_cam = cam(input_tensor=input_tensor, targets=targets)
-        grayscale_cam = grayscale_cam[0, :]
-
-        # Get back the original image
-        img = input_tensor.squeeze(0).to('cpu')
-        img = inv_normalize(img)
-        rgb_img = np.transpose(img, (1, 2, 0))
-        rgb_img = rgb_img.numpy()
-
-        # Mix the activations on the original image
-        visualization = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True, image_weight=transparency)
-
-        # Display the images on the plot
-        plt.imshow(visualization)
-        plt.title(r"Correct: " + classes[data[i][1].item()] + '\n' + 'Output: ' + classes[data[i][2].item()])
-        plt.xticks([])
-        plt.yticks([])
 
 
-def display_cifar_misclassified_data(data: list,
-                                     classes: list[str],
-                                     inv_normalize: transforms.Normalize,
-                                     number_of_samples: int = 10):
-    """
-    Function to plot images with labels
-    :param data: List[Tuple(image, label)]
-    :param classes: Name of classes in the dataset
-    :param inv_normalize: Mean and Standard deviation values of the dataset
-    :param number_of_samples: Number of images to print
-    """
-    fig = plt.figure(figsize=(10, 10))
 
-    x_count = 5
-    y_count = 1 if number_of_samples <= 5 else math.floor(number_of_samples / x_count)
 
-    for i in range(number_of_samples):
-        plt.subplot(y_count, x_count, i + 1)
-        img = data[i][0].squeeze().to('cpu')
-        img = inv_normalize(img)
-        plt.imshow(np.transpose(img, (1, 2, 0)))
-        plt.title(r"Correct: " + classes[data[i][1].item()] + '\n' + 'Output: ' + classes[data[i][2].item()])
-        plt.xticks([])
-        plt.yticks([])
+
